2a17baf717c5c666e922ba75552c389768ea54db
dipanker <dipankerkumar2010@gmail.com>
2023-03-05 16:48:31 +0530

    initial commit

--- java-basic/src/main/java/com/example/javabasic/JavaBasicApplication.java
+++ java-basic/src/main/java/com/example/javabasic/JavaBasicApplication.java
@@ -8,7 +8,7 @@ public class JavaBasicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaBasicApplication.class, args);
-        System.out.println("Spring boot application started.");
+        System.out.println("Spring boot application started successfully.");
     }
 
 }
